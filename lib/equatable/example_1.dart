b4060ba4d2f67077acac442cde67f42dabd6d9c2
ismail <ismailcoder366@gmail.com>
2026-02-20 14:22:41 +0500

    equatable concept added

--- lib/equatable/example_1.dart
+++ lib/equatable/example_1.dart
@@ -1,10 +1,15 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
-class Person {
+class Person extends Equatable{
   String name;
   int age;
 
   Person(this.name, this.age);
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [name, age];
 
 }
 
@@ -18,7 +23,12 @@ class Example1 extends StatelessWidget {
         Person person = Person('ismail', 24);
         Person person1 = Person('ismail', 24);
 
-        print(person == person1);
+        print(person == person1); // these are not equal bcs in flutter only instance is equal to itself and here we have two instances person and person1
+        print(person == person);  // these are now equal
+
+        /// checking equality through hashcode
+        print(person.hashCode.toString());
+        print(person1.hashCode.toString());
       },child: Text('Add')),
     );
   }
